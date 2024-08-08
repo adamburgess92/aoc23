f9886c2289f49955045029eb05ae8f3ef410b1f5
Adam Burgess <adam.burgess92@gmail.com>
2024-08-08 02:09:21 +0000

    Day 3 solved

--- src/main/scala/day3_1.scala
+++ src/main/scala/day3_1.scala
@@ -9,25 +9,20 @@ object DayThreePartOne {
         val atomised_grid = atomiseGrid(data)
         // Part 1:
         val p = getRegexLocations(data, """\d+""".r)
-        // val res = getResultPartOne(atomised_grid, p)
-        // println(res)
-        // Part 2:
-        val q = getRegexLocations(data, """\*""".r)
-        p.foreach(println)
-        println()
-        q.foreach(println)
+        val res = getResultPartOne(atomised_grid, p)
+        println(res)
     }
 }
 
-def getRegexLocations(ls_s: List[String], regex_pattern:Regex): List[Tuple3[String, List[Int], List[Int]]] = {
-    def inner(s: List[String], row_num: Int, a:List[Tuple3[String, List[Int], List[Int]]]): List[Tuple3[String, List[Int], List[Int]]] = s match {
+def getRegexLocations(ls_s: List[String], regex_pattern:Regex): List[Tuple3[String, (Int, Int), (Int, Int)]] = {
+    def inner(s: List[String], row_num: Int, a:List[Tuple3[String, (Int, Int), (Int, Int)]]): List[Tuple3[String, (Int, Int), (Int, Int)]] = s match {
         case h::t => { // h looks like "467..114..""
-            val tpl = regex_pattern.findAllMatchIn(h).map(m => Tuple3(m.matched, List(row_num, m.start), List(row_num, m.end-1)))
+            val tpl = regex_pattern.findAllMatchIn(h).map(m => Tuple3(m.matched, (row_num, m.start), (row_num, m.end-1)))
             inner(t, row_num+1, a++tpl)
         }
         case Nil => a
     }
-    val a: List[(String, List[Int], List[Int])] = Nil
+    val a: List[(String, (Int, Int), (Int, Int))] = Nil
     inner(ls_s, 0, a)
 }
 
@@ -53,11 +48,11 @@ def isSpecialCharacter(c: Option[Char]): Boolean = c match {
     case None => false
 }
 
-def checkIfAdjacent(atomised_grid: List[List[Char]], number_location: Tuple3[String, List[Int], List[Int]]): Int = {
+def checkIfAdjacent(atomised_grid: List[List[Char]], number_location: Tuple3[String, (Int, Int), (Int, Int)]): Int = {
     val number = number_location._1.toInt
-    val row = number_location._2.head
-    val start_col = number_location._2.last
-    val end_col = number_location._3.last
+    val row = number_location._2._1
+    val start_col = number_location._2._2
+    val end_col = number_location._3._2
     val cols = (start_col to end_col).toList
     // Iterate: 
     def inner(row: Int, ls_cols: List[Int], a:Int): Int = ls_cols match {
@@ -86,40 +81,10 @@ def checkIfAdjacent(atomised_grid: List[List[Char]], number_location: Tuple3[Str
     if (res>0) number else 0
 }
 
-def getResultPartOne(atomised_grid: List[List[Char]], ls_number_location: List[Tuple3[String, List[Int], List[Int]]]): Int = {
-    def inner(ls_number_location: List[Tuple3[String, List[Int], List[Int]]], a: Int): Int = ls_number_location match {
+def getResultPartOne(atomised_grid: List[List[Char]], ls_number_location: List[Tuple3[String, (Int, Int), (Int, Int)]]): Int = {
+    def inner(ls_number_location: List[Tuple3[String, (Int, Int), (Int, Int)]], a: Int): Int = ls_number_location match {
         case h::t => inner(t, a+checkIfAdjacent(atomised_grid, h))
         case Nil => a
     }
     inner(ls_number_location, 0)
 }
-
-def getResultPartTwo(ls_gear: List[Tuple3[String, List[Int], List[Int]]], ls_nums: List[Tuple3[String, List[Int], List[Int]]]): Unit = {
-    ls_nums.foreach(println)
-    ls_gear.foreach(println)
-}
-
-// def getResultPartTwo(ls_gear: List[Tuple3[String, List[Int], List[Int]]], ls_nums: List[Tuple3[String, List[Int], List[Int]]]): Int = {
-//     ls_nums.foreach(println)
-//     ls_gear.foreach(println)
-//     def inner(ls_gear: List[Tuple3[String, List[Int], List[Int]]], ls_nums: List[Tuple3[String, List[Int], List[Int]]], a:Int, count:Int): Int = ls_gear match {
-//         """
-//         a : accumulator
-//         count : count adjacent ints. If >2, return 0.
-//         """
-//         case h::t => {
-//             val gear_row = h._2.head
-//             val gear_col = h._2.last
-//             val b = compare(gear_row, gear_col, ls_nums, 0)
-//             inner(t, ls_nums, a+b)
-//         }
-//         case Nil => a
-//     }
-//     def compare(gear_row: Int, gear_col: Int, ls_nums: List[Tuple3[String, List[Int], List[Int]]], a:Int): Int = ls_nums match {
-//         case h::t => {
-
-//         }
-//         case Nil => a
-//     }
-//     inner(ls_gear, ls_nums, 1, 0)
-// }
