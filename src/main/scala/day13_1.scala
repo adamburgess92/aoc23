2fd88c74a96cfd8dfa170438a323dee53d935fbe
Adam Burgess <adam.burgess92@gmail.com>
2024-08-14 04:25:54 +0000

    Day 13 solved

--- src/main/scala/day13_1.scala
+++ src/main/scala/day13_1.scala
@@ -3,12 +3,13 @@ package day13
 import scala.util.boundary, boundary.break
 import utils.Utils
 import scala.io.Source
+import day4.numMatch
 
 object DayThirteenPartOne {
     def main(args: Array[String]): Unit = {
         // Actual
         val blocks = readBlocks("day13/test_data.txt")
-        val res = processAllBlocks(blocks)
+        val res = processAllBlocks(blocks, assertMatch)
         println(res)
     }
 }
@@ -52,11 +53,39 @@ def transposeData(data: List[String]): List[String] = {
     gridT.map(j=>j.mkString)
 }
 
-// def assertMatch(z: List[Tuple]): Boolean = {
-//     z.forall { case (a, b) => a==b}
-// }
+def assertMatch(z: List[(String, String)]): Boolean = {
+    z.forall { case (a, b) => a==b}
+}
 
-def getReflectPointHorizontal(data: List[String], multiplier: Int): Option[Int] = boundary {
+def assertSmudgeMatch(z: List[(String, String)]): Boolean = {
+    if (assertMatch(z)) { // Needs to be a different reflection point
+        false 
+    } else {
+        // Iterate over z: 
+        val zLength = z.length
+        def inner(z: List[(String, String)], nMatch: Int, nDiscrep: Int): (Int, Int) = z match {
+            case h::t =>  { 
+                // h = (#...##..#, #....#..#)
+                val isMatch = h._1 == h._2 // exact match
+                val smudgeMatch = h._1.zip(h._2).count( { case (a, b) => a!=b} ) == 1
+
+                if (isMatch) {
+                    inner(t, nMatch+1, nDiscrep)
+                } else if (smudgeMatch) {
+                    inner(t, nMatch, nDiscrep+1)
+                } else {
+                    inner(t, nMatch, nDiscrep)
+                }
+            }
+            case Nil => (nMatch, nDiscrep)
+        }
+        val (nMatch, nDiscrep) = inner(z, 0, 0)
+        if (nMatch==zLength-1 && nDiscrep==1) true else false
+    }
+}
+
+
+def getReflectPointHorizontal(data: List[String], multiplier: Int, func: (z: List[(String, String)]) => Boolean): Option[Int] = boundary {
     for (i <- 0 to data.length){
         val above = data.slice(0, i)
         val below = data.slice(i, data.length)
@@ -69,8 +98,7 @@ def getReflectPointHorizontal(data: List[String], multiplier: Int): Option[Int] 
                 below.zip(revAbove)
             }
         }
-        val allMatch = z.forall { (a, b) => a==b}
-        println(z)
+        val allMatch = func(z)
         if (allMatch && z.nonEmpty) {
             break(Some((i)*multiplier))
         }
@@ -78,16 +106,16 @@ def getReflectPointHorizontal(data: List[String], multiplier: Int): Option[Int] 
     None
 }
 
-def getReflectionScore(data: List[String]): Int = {
+def getReflectionScore(data: List[String], func: (z: List[(String, String)])=>Boolean): Int = {
     val dataT = transposeData(data)
-    getReflectPointHorizontal(dataT, 1).getOrElse {
-        getReflectPointHorizontal(data, 100).getOrElse(0)
+    getReflectPointHorizontal(dataT, 1, func).getOrElse { // Check for vertical reflection by transposing data
+        getReflectPointHorizontal(data, 100, func).getOrElse(0) // Check for horizontal reflection
     }
 }
 
-def processAllBlocks(dataBlocks: List[List[String]]): Int = {
+def processAllBlocks(dataBlocks: List[List[String]], func:(z:List[(String, String)])=>Boolean): Int = {
     def inner(dataBlocks: List[List[String]], a: Int): Int = dataBlocks match {
-        case h::t => inner(t, a+getReflectionScore(h))
+        case h::t => inner(t, a+getReflectionScore(h, func))
         case Nil => a
     }
     inner(dataBlocks, 0)
