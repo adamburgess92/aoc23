75905a974bb01e9ed8b2123054c2b9fb6844d11a
Adam Burgess <adam.burgess92@gmail.com>
2024-08-14 07:48:50 +0000

    Day 14 Part 1 solved

---
+++ src/main/scala/day14_1.scala
@@ -0,0 +1,83 @@
+package day14
+
+import utils.Utils
+
+object DayFourteenPartOne {
+    def main(args: Array[String]): Unit = {
+        val data = Utils.readLines("day14/test_data.txt")
+        val grid = atomiseData(data)
+        val tiltedGrid = exhaustMoveStones(grid)
+        val res = calcLoad(tiltedGrid)
+        println(res)
+    }
+}
+
+def atomiseData(data: List[String]): List[List[Char]] = {
+    def inner(data: List[String], a: List[List[Char]]): List[List[Char]] = data match {
+        case h::t => inner(t, a:+h.toList)
+        case Nil => a
+    }
+    val aInit: List[List[Char]] = Nil
+    inner(data, aInit)
+}
+
+def queryUp(grid: List[List[Char]], i: Int, j: Int): Option[Char] = {
+    try {
+        Some(grid(i-1)(j))
+    } catch {
+        case e: IndexOutOfBoundsException => None
+    }
+}
+
+def moveUpPossible(grid: List[List[Char]], i: Int, j:Int): Boolean = {
+    grid(i)(j)=='O' && queryUp(grid, i, j).contains('.')
+}
+
+def moveStone(grid: List[List[Char]], i: Int, j: Int): (List[List[Char]], Int) = {
+    val (newGrid, nMoved) = if (moveUpPossible(grid, i, j)) {
+        val updatedRowAbove = grid(i-1).updated(j, 'O')
+        val updatedRow = grid(i).updated(j, '.')
+        (grid.updated(i-1, updatedRowAbove).updated(i, updatedRow), 1)
+    } else {
+        (grid, 0)
+    }
+    (newGrid, nMoved)
+}
+
+def moveAllStonesByOne(grid: List[List[Char]]): (List[List[Char]], Int)= {
+    val i = (0 to grid.length-1).toList
+    val j = (0 to grid(0).length-1).toList
+    val z = i.flatMap(x => j.map(y => (x, y)))
+    def inner(z: List[(Int, Int)], g: List[List[Char]], nMoves:Int): (List[List[Char]], Int) = z match {
+        case h::t => {
+            val (newGrid, nMoved) = moveStone(g, h._1, h._2)
+            inner(t, newGrid, nMoves+nMoved)
+        }
+        case Nil => (g, nMoves)
+    }
+    val (newGrid, nMoves) = inner(z, grid, 0)
+    (newGrid, nMoves)
+}
+
+def exhaustMoveStones(grid: List[List[Char]]): List[List[Char]] = {
+    val (newGrid, nMoves) = moveAllStonesByOne(grid)
+    println(s"Moved $nMoves stones")
+    if (nMoves==0) {
+        newGrid
+    } else {
+        exhaustMoveStones(newGrid)
+    }
+}
+
+def calcLoad(tiltedGrid: List[List[Char]]): Int = {
+    def inner(tiltedGrid: List[List[Char]], w:Int, a:Int): Int = tiltedGrid match {
+        case h::t => {
+            // h = List(O,O,O,O,.,#,.,O,.,.)
+            // Count stones
+            val nStones = h.count(i=> i=='O')
+            inner(t, w-1, a+nStones*w)
+        }
+        case Nil => a
+    }
+    inner(tiltedGrid, tiltedGrid.length, 0)
+}
